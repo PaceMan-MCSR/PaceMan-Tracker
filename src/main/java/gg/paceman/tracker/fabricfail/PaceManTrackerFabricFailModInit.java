2495a503dcbeb85790d939a7b244ecbf573a143f
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-03-24 14:23:44 -0300

    Remove fabric dependency

--- src/main/java/gg/paceman/tracker/fabricfail/PaceManTrackerFabricFailModInit.java
+++ src/main/java/gg/paceman/tracker/fabricfail/PaceManTrackerFabricFailModInit.java
@@ -1,9 +1,6 @@
 package gg.paceman.tracker.fabricfail;
 
-import net.fabricmc.api.ModInitializer;
-
-public class PaceManTrackerFabricFailModInit implements ModInitializer {
-    @Override
+public class PaceManTrackerFabricFailModInit {
     public void onInitialize() {
         throw new RuntimeException("PaceMan Tracker is not supposed to be ran as a mod!");
     }
